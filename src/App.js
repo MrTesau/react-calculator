6f23663db7a99a29fa7c49499164bf7c313a7c86
Tayyib Esau <esamu161@gmail.com>
2020-11-07 16:18:27 +1300

    updated react component, added styles

--- src/App.js
+++ src/App.js
@@ -1,96 +1,58 @@
-/* SNIPPETS
-numberInput={this.numberInput} 
-onClick={this.props.numberInput()}
+import React from "react";
+import "./styles.css";
 
-Reference: https://github.com/MyNameIsURL/react-calculator-app/blob/master/src/App.css
-*/
-class AppComponent extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      userInput: "25*3",
-      displayAnswer: "",
-      buttons: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "AC", "+", "-", "/", "=", "."],
-    };
-  }
-  // function to handle number inputs
-  input = (event) => {
-    // clear
-    if (event.target.value === "AC") {
-      this.setState({
-        userInput: "",
-      });
-    } else if (event.target.value === "=") {
-      this.setState({
-        userInput: math.evaluate(this.state.userInput),
-        // case for invalid inputs
-        // double *
-        //double /
-      });
-    } else {
-      this.setState({
-        userInput: this.state.userInput + event.target.value,
-      });
-    }
+const App = () => {
+  const [display, setDisplay] = React.useState("0");
+  // const [operations, setOperation] = React.useState([]);
+
+  const inputHandler = (input) => {
+    // eslint-disable-next-line
+    display == "0" ? setDisplay(input) : setDisplay(`${display}${input}`);
+  };
+  const operatorHandler = (operator) => {
+    // let str = String(display).substr(0, display.length - 1);
+
+    isNaN(parseInt(display[display.length - 1]))
+      ? setDisplay(
+          `${String(display).substr(0, display.length - 1)}${operator}`
+        )
+      : setDisplay(`${display}${operator}`);
   };
 
-  render() {
-    // render button component for each item in numbers arr
-    // individually render these components instead
-    const numbers = this.state.buttons.map((i) => (
-      <ButtonComponent buttVal={i} input={this.input} />
-    ));
-
-    // [1, 2, 3, 4, 5, 6, 7, 8, 9, 0,"AC","+", "-", "/", "=", ".", "*"]
-    return (
-      <div className="cont">
-        <div className="calc-wrapper">
-          <Input input={this.state.userInput} />
-          <div className="row">
-            <ButtonComponent buttVal={1} input={this.input} />
-            <ButtonComponent buttVal={2} input={this.input} />
-            <ButtonComponent buttVal={3} input={this.input} />
-            <ButtonComponent buttVal={4} input={this.input} />
-          </div>
-          <div className="row">
-            <ButtonComponent buttVal={5} input={this.input} />
-            <ButtonComponent buttVal={6} input={this.input} />
-            <ButtonComponent buttVal={7} input={this.input} />
-            <ButtonComponent buttVal={8} input={this.input} />
-          </div>
-          <div className="row">
-            <ButtonComponent buttVal={9} input={this.input} />
-            <ButtonComponent buttVal={0} input={this.input} />
-            <ButtonComponent buttVal={"AC"} input={this.input} />
-            <ButtonComponent buttVal={"="} input={this.input} />
-          </div>
-          <div className="row">
-            <ButtonComponent buttVal={"-"} input={this.input} />
-            <ButtonComponent buttVal={"/"} input={this.input} />
-            <ButtonComponent buttVal={"+"} input={this.input} />
-            <ButtonComponent buttVal={"*"} input={this.input} />
-          </div>
-        </div>
+  return (
+    <div className="flex-container">
+      <div className="container">
+        <div className="display">{display}</div>
+        <button onClick={() => inputHandler(1)}>1</button>
+        <button onClick={() => inputHandler(2)}>2</button>
+        <button onClick={() => inputHandler(3)}>3</button>
+        <button onClick={() => inputHandler(4)}>4</button>
+        <button onClick={() => inputHandler(5)}>5</button>
+        <button onClick={() => inputHandler(6)}>6</button>
+        <button onClick={() => inputHandler(7)}>7</button>
+        <button onClick={() => inputHandler(8)}>8</button>
+        <button onClick={() => inputHandler(9)}>9</button>
+        <button onClick={() => inputHandler(0)}>0</button>
+        <button onClick={() => inputHandler(".")}>.</button>
+        <button onClick={() => operatorHandler("+")}>+</button>
+        <button onClick={() => operatorHandler("/")}>/</button>
+        <button onClick={() => operatorHandler("-")}>-</button>
+        <button onClick={() => operatorHandler("*")}>x</button>
+        <button
+          className="big-flex"
+          onClick={() => {
+            // eslint-disable-next-line
+            setDisplay(`${eval(display)}`);
+          }}
+        >
+          =
+        </button>
+        <button className="big-flex" onClick={() => setDisplay(0)}>
+          AC
+        </button>
       </div>
-    );
-  }
-}
-
-class ButtonComponent extends React.Component {
-  render() {
-    let val = this.props.buttVal;
-    return (
-      <button className="button-wrapper" onClick={this.props.input} value={val}>
-        {val}
-      </button>
-    );
-  }
-}
-
-const Input = (props) => {
-  return <div className="input">{props.input}</div>;
+    </div>
+  );
 };
-// component for actions
-// uses different event functions than number
 
-export default AppComponent;
+export default App;
